c52bc01851ebb80903245de6a6528c441bccba5c
erik-cahya <erik.cahya841@gmail.com>
2025-02-04 18:23:02 +0800

    update form

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 export const Test = () => {
   return (
-    <div>
-        <h1>Contact Us</h1>
-        <form 
-        name="subscripe" method="POST" 
+    <div style={{ paddingTop: '200px', paddingBottom: '100px', paddingLeft: '20px' }}>
+      <h1>Contact Us</h1>
+      <form 
+        name="subscribe" 
+        method="POST" 
+        action="/success"
         data-netlify="true"
-        data-netlify-honeypot="bot-field">
-            
-        <input type="hidden" name="form-name" value="subscripe" />
-    
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="subscribe" />
+        <input type="hidden" name="bot-field" />
+
         <div>
-            <label>
-            Email: <input type="email" name="email"/>
-            </label>
+          <label>
+            Email: 
+            <input type="email" name="email" required style={{ border: '1px solid black' }} />
+          </label>
         </div>
-        
+
         <div>
-            <button type="submit">Send</button>
+          <button type="submit" style={{ border: '1px solid black' }}>Send</button>
         </div>
-        </form>
+      </form>
     </div>
-  )
-}
-export default Test
+  );
+};
+
+export default Test;
