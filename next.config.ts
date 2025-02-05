4cd4a3fcdadfa31caf5c145163d2113c9f19d5ab
erik-cahya <erik.cahya841@gmail.com>
2025-02-06 01:12:26 +0800

    update (catalogues) : separate slug folder & files

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Keep strict mode enabled
   experimental: {}, // Ensure there are no unnecessary keys
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true, // ✅ Disable Image Optimization for static export
   },
