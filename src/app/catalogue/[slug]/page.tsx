9ca4d254cea6d57062e89e80bb2289acb7e47a91
erik-cahya <erik.cahya841@gmail.com>
2025-02-06 01:28:10 +0800

    update

--- src/app/catalogue/[slug]/page.tsx
+++ src/app/catalogue/[slug]/page.tsx
@@ -2,23 +2,25 @@ import { notFound } from "next/navigation";
 import { CATALOGUE } from '@/constants/villa';
 import CatalogueClient from "./CatalogueClient";
 
+// Ensure generateStaticParams returns a promise
 export async function generateStaticParams() {
-    return CATALOGUE.map((item) => ({
-        slug: item.slug,
-    }));
+  return Promise.resolve(
+    CATALOGUE.map((item) => ({
+      slug: item.slug,
+    }))
+  );
+}
+
+const CatalogueDetail = ({ params }: { params: { slug: string } }) => {
+  const { slug } = params;
+
+  const catalogue = CATALOGUE.find((item) => item.slug === slug);
+
+  if (!catalogue) {
+    return notFound();
   }
 
-  const CatalogueDetail = ({ params }: { params: { slug: string } }) => {
-    const { slug } = params;
-
-    const catalogue = CATALOGUE.find((item) => item.slug === slug);
-
-    if (!catalogue) {
-        return notFound();
-    }
-
-    return <CatalogueClient catalogue={catalogue} />;
+  return <CatalogueClient catalogue={catalogue} />;
 };
 
 export default CatalogueDetail;
-
