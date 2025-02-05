7ac9af2f2cde17268c7a97d2d63d6ac9f33daea7
erik-cahya <erik.cahya841@gmail.com>
2025-02-06 01:18:35 +0800

    update

--- src/app/catalogue/[slug]/page.tsx
+++ src/app/catalogue/[slug]/page.tsx
@@ -2,14 +2,13 @@ import { notFound } from "next/navigation";
 import { CATALOGUE } from '@/constants/villa';
 import CatalogueClient from "./CatalogueClient";
 
-// Gunakan generateStaticParams untuk menangani url
 export function generateStaticParams() {
   return CATALOGUE.map((item) => ({
       slug: item.slug,
   }));
 }
 
-const CatalogueDetail = ({ params }: { params: { slug: string } }) => {
+const CatalogueDetail = async ({ params }: { params: { slug: string } }) => {
     const { slug } = params;
 
     const catalogue = CATALOGUE.find((item) => item.slug === slug);
